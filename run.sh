7eb7a63ac35e97934e45cf0923f737a149fbd74e
Simeon Widdis <sawiddis@gmail.com>
2023-12-05 20:20:12 -0800

    Add self-cd to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Navigate to directory where the script is located
+# Allows running from cron directly
+cd "$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
 python3 -m venv venv
 source ./venv/bin/activate
 pip install -r requirements.txt
